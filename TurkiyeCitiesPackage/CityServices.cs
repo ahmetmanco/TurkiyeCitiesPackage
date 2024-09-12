dbb35d991ea9f4c9d7fed9078f6feeb2f431265f
ahmetmanco <ahmetmanco07@gmail.com>
2024-09-13 00:33:46 +0300

    Add project files.

---
+++ TurkiyeCitiesPackage/CityServices.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace TurkiyeCitiesPackage
+{
+    public class CityServices
+    {
+        private readonly List<City> _cityList;
+
+        public CityServices()
+        {
+            var citiesJson = File.ReadAllText("Cities.json");
+            _cityList = JsonConvert.DeserializeObject<List<City>>(citiesJson);
+        }
+        public List<City> GetAllCities()
+        {
+            return _cityList;
+        }
+
+        public City GetCityById(int id)
+        {
+            return _cityList.FirstOrDefault(x => x.Id == id);
+        }
+        public List<Districts> GetDistrictsByCityId(int cityId)
+        {
+            var city = _cityList.FirstOrDefault(c => c.Id == cityId);
+            return city?.Districts;
+        }
+    }
+}
---
+++ TurkiyeCitiesPackage/README.md
@@ -0,0 +1,6 @@
+﻿# Description
+Bu Türkiye'nin illerinin ve ilçelerinin bulunduğu paket dosyasıdır.
+
+#install 
+
+Bu paketi Nuget Package Manager üzerinden "TurkiyeCitiesPackage" aratarak, ilgili katman içine indirebilirsiniz.
